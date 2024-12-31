9d82bfad4d9bc6a9a6080810d211444ab3b6e5ec
Gurpreet Singh <1221320gurpreet@gmail.com>
2024-12-31 15:27:04 +0530

    asd

--- components/LightboxPdf/index.tsx
+++ components/LightboxPdf/index.tsx
@@ -101,8 +101,7 @@ const MainComponent = ({ buttonText, pdfUrl, extraclass }) => {
                     onClick={() => setPageNumber(pageNumber + 1)}
                     disabled={pageNumber >= numPages}
                     className="px-4 py-2 bg-blue-500 text-white rounded disabled:opacity-50"
-                  >
-                    Next
+                  >  Next
                   </button>
                 </div>
               )}
