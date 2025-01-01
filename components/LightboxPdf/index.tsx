481b8e412b465d69cb63fe511e5d645d9b842cac
Gurpreet Singh <1221320gurpreet@gmail.com>
2025-01-01 13:31:45 +0530

    checking v2

--- components/LightboxPdf/index.tsx
+++ components/LightboxPdf/index.tsx
@@ -3,8 +3,10 @@ import React, { useState, useEffect, useRef } from "react";
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/TextLayer.css';  // Import the styles for the TextLayer
 
-
+import '/pdf.worker.min.mjs';
 pdfjs.GlobalWorkerOptions.workerSrc = `/pdf.worker.min.mjs`;
+
+
 import '@react-pdf-viewer/core/lib/styles/index.css'; 
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 const MainComponent = ({ buttonText, pdfUrl, extraclass }) => {
