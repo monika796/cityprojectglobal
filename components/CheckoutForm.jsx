c971937116c28e56a3107be374f57b1bdbe3fbdf
Kamaldhiman07 <122001124+Kamaldhiman07@users.noreply.github.com>
2025-01-03 17:05:02 +0530

    Update CheckoutForm.jsx

--- components/CheckoutForm.jsx
+++ components/CheckoutForm.jsx
@@ -28,7 +28,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:3000/complete-payment",
+        return_url: "https://citiesprojectglobal.vercel.app/donation-second",
       },
     });
 
