e09f4a7ec500c9e9056a114538f7184da5c45d26
monikagupta <monikagupta796@gmail.com>
2025-01-03 23:24:56 +0530

    check

--- app/(site)/leadershipcircle-form/page.tsx
+++ app/(site)/leadershipcircle-form/page.tsx
@@ -5,7 +5,7 @@ import DobGender from "@/components/multiformstep/Stepsecond";
 import Address from "@/components/multiformstep/Stepthird";
 import Image from "next/image";
 import axios from "axios";
-export const revalidate = 60 // revalidate at most every 5 minutes
+
 const MainForm = () => {
   const [data, setData] = useState({
     input_5: "", // Email
